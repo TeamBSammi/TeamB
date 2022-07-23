afa8babe75bafe41300e2204a763c205047e97a9
TeamBSammi <s10247843@connect.np.edu.sg>
2022-07-24 01:34:31 +0800

    api

--- mainsolution.py
+++ mainsolution.py
@@ -2,14 +2,16 @@ import requests
 from pathlib import Path
 import re, csv
 
-file_path = Path.cwd()/"TeamB"
-#file_csv = Path.cwd()/"TeamB"/"tenancy.csv"
-#file_csv.touch()
-
-url = "https://github.com/TeamBSammi/TeamB.git"
+url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATEfrom_symbol=USDto_symbol=SGD"
 response = requests.get(url)
 print(response)
-
 data = response.json()
 print(data)
+
+file_path = Path.cwd()/"mainsolution.py"
+file_path2 = Path.cwd()/"mainsolution.py"/"currencyexchangerate.txt"
+file_path2.touch()
+print(file_path2.exists())
+
+
 
