3cc7ff511a6a7c19dfe76fc3f66a072e8fb3830a
TeamBSammi <s10247843@connect.np.edu.sg>
2022-07-24 01:13:19 +0800

    api

---
+++ mainsolution.py
@@ -0,0 +1,15 @@
+import requests
+from pathlib import Path
+import re, csv
+
+file_path = Path.cwd()/"TeamB"
+file_csv = Path.cwd()/"TeamB"/"tenancy.csv"
+file_csv.touch()
+
+url = "https://github.com/TeamBSammi/TeamB.git"
+response = requests.get(url)
+print(response)
+
+data = response.json()
+print(data)
+
