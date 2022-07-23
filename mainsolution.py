9c1c31bc7673fa67bfff7e093ff212ee5879187d
TeamBSammi <s10247843@connect.np.edu.sg>
2022-07-24 01:34:25 +0800

    api

--- mainsolution.py
+++ mainsolution.py
@@ -3,8 +3,8 @@ from pathlib import Path
 import re, csv
 
 file_path = Path.cwd()/"TeamB"
-file_csv = Path.cwd()/"TeamB"/"tenancy.csv"
-file_csv.touch()
+#file_csv = Path.cwd()/"TeamB"/"tenancy.csv"
+#file_csv.touch()
 
 url = "https://github.com/TeamBSammi/TeamB.git"
 response = requests.get(url)
