6b1c02d106a76bafeac88da3bef37cf340e56076
teamB_jialing <s10221087@connect.np.edu.sg>
2022-07-24 01:52:05 +0800

    api

--- mainsolution.py
+++ mainsolution.py
@@ -1,8 +1,8 @@
-
 import requests
 from pathlib import Path
 import csv
 import re
+
 url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=RRIW0YM0PRPMKD9Q'
 response = requests.get(url)
 print(response)
@@ -11,7 +11,3 @@ print(data)
 
 file_path = Path.cwd()/"mainsolution.py"
 file_path2 = Path.cwd()/"mainsolution.py"/"summary_report.txt"
-
-
-
-
