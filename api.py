2ee72e1fbccaa60e0e37eda1f2b9fa0055fb8c22
teamB_jialing <s10221087@connect.np.edu.sg>
2022-07-24 18:04:14 +0800

    api

--- api.py
+++ api.py
@@ -1,1 +1,10 @@
-jialing = "jiayu"
+import requests
+from pathlib import Path
+import csv
+import re
+
+url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=RRIW0YM0PRPMKD9Q'
+response = requests.get(url)
+print(response)
+data = response.json()
+print(data)
