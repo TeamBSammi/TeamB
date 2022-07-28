356c209d5a23059e5bbec6b5cb187e5e1d17bbc5
TeamBSammi <s10247843@connect.np.edu.sg>
2022-07-28 15:47:20 +0800

    cohread

---
+++ read_file.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+from pathlib import Path
+import re, csv
+
+file_cohpath = Path.cwd()/"TeamB"/"csv_reports"/"cash-on-hand-usd.csv"
+
+for file in file_cohpath.glob("*.txt"):
+    with file.open(mode = "r", encoding = "utf-8",) as fileread:
+
+        csvreader = csv.reader(fileread)
+        print(csvreader)
