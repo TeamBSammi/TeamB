596d0e3b99c558df67b1101d0b30618e5b8d39f4
TeamBSammi <s10247843@connect.np.edu.sg>
2022-07-28 17:23:29 +0800

    read file coh

--- read_file.py
+++ read_file.py
@@ -2,9 +2,11 @@ from dataclasses import dataclass
 from pathlib import Path
 import re, csv
 
-file_cohpath = Path.cwd()/"TeamB"/"csv_reports"/"cash-on-hand-usd.csv"
+file_cohpath = Path.cwd()/"csv_reports"/"cash-on-hand-usd.csv"
 
-with file_cohpath.open(mode = "r", encoding = "utf-8",) as fileread:
+with file_cohpath.open(mode = "r", encoding = "utf-8") as fileread:
     csvreader = csv.reader(fileread)
     next(csvreader)
+    date = []
+    amount = []
 print(csvreader)
