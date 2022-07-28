f264e9992c7008ee38093af0375faeb8e894c986
TeamBSammi <s10247843@connect.np.edu.sg>
2022-07-28 17:23:23 +0800

    read file coh

--- read_file.py
+++ read_file.py
@@ -4,9 +4,7 @@ import re, csv
 
 file_cohpath = Path.cwd()/"TeamB"/"csv_reports"/"cash-on-hand-usd.csv"
 
-for file in file_cohpath.glob("*.txt"):
-    with file.open(mode = "r", encoding = "utf-8",) as fileread:
-
-        csvreader = csv.reader(fileread)
-        
+with file_cohpath.open(mode = "r", encoding = "utf-8",) as fileread:
+    csvreader = csv.reader(fileread)
+    next(csvreader)
 print(csvreader)
