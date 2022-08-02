f6ed650e7219ee3b2d2dd3856ba9e743d22974be
TeamBSammi <s10247843@connect.np.edu.sg>
2022-08-02 17:26:07 +0800

    j

---
+++ cash_on_hand.py
@@ -0,0 +1,38 @@
+#reading cash on hand file
+from pathlib import Path
+import re, csv
+
+file_cohpath = Path.cwd()/"csv_reports"/"cash-on-hand-usd.csv"
+
+#to read the file
+with file_cohpath.open(mode = "r", encoding = "utf-8") as fileread:
+    csvreader = csv.reader(fileread)
+    next(csvreader)
+    #creating list with no elements
+    day = []
+    coh = []
+
+    #as day in the CSV is the first value before the cash on hand it will be 0 value while amount will be 1
+    for values in csvreader:
+        day.append(values[0])
+        coh.append(values[1])
+coh1 = 0
+coh2 = -1
+difference = []
+#looping the values. as there are 6 values in total, the range is 0 to 5
+for number in range(0,5):
+    coh1 += 1
+    coh2 += 1
+    #the formula to calculate the difference of the cash on hand integers
+    difference.append(int(coh[coh1])-(int(coh[coh2])))
+print(difference)
+for day,number in enumerate(difference,45):
+    #this formula is for when the number captured is negative after the difference is caluclated to be used
+    if number < 0:
+        #to print the data and turn the negative cash on hand number to positive
+        print(f"[CASH DEFICT] US${abs(number)} on day {day}")
+    elif number > 0:
+        #to print the data and turn the negative cash on hand number to positive
+        print(f"[CASH SURPLUS] US${abs(number)} on day {day}")
+
+
