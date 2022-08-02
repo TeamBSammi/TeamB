fe4f71d6a8cd0b386ba2aec8c61d2bb167a39d67
teamB_jialing <s10221087@connect.np.edu.sg>
2022-08-02 16:54:55 +0800

    changing overheads to day 40

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ data = response.json()
 print(data)
 
 file_path = Path.cwd()/"csv_reports"
-file_path2 = Path.cwd()/"csv_reports"/"TeamB"/"summary_report.txt"
+file_path2 = Path.cwd()/"csv_reports"/"summary_report.txt"
 file_path2.touch()
 print(file_path2.exists())
 
@@ -19,8 +19,9 @@ file_path2.touch()
 if file_path2.exists():
     with file_path2.open('w', encoding = 'UTF-8', newline = '') as file:
         writer = csv.writer(file)
+        test = "test = "
         for item in data.values():
-             writer.writerow([item["5. Exchange Rate"]]) 
+             writer.writerow([item["5. Exchange Rate"]])
         
     file.close()
 
