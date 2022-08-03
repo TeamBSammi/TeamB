1b8bb6437bde2889dffd1a2c2c25dba7f35b166e
ewnice <eunicechua72@gmail.com>
2022-08-03 19:08:47 +0800

    writelines2

--- read_file.py
+++ read_file.py
@@ -57,10 +57,10 @@ txtfile.touch()
 with txtfile.open(mode = "w",newline = "") as file:
     for day,number in enumerate(difference,45):
         if number < 0:
-            file.writelines(f"[CASH DEFICT] US${abs(number)} on day {day}")
+            file.writelines(f"[CASH DEFICT] US${abs(number)} on day {day}\n")
     for day,number in enumerate(diff,45):
         if number < 0:
-            file.writelines(f"[PROFIT DEFICT] US${abs(number)} on day {day}")
+            file.writelines(f"[PROFIT DEFICT] US${abs(number)} on day {day}\n")
 
 
 
@@ -88,8 +88,8 @@ print(f"[HIGHEST OVERHEADS] {cat}: {maxover}")
 with txtfile.open(mode = "w",newline = "") as file:
     for day,number in enumerate(difference,45):
         if number < 0:
-            file.writelines(f"[CASH DEFICT] US${abs(number)} on day {day}")
+            file.writelines(f"[CASH DEFICT] US${abs(number)} on day {day}\n")
     for day,number in enumerate(diff,45):
-        file.writelines(f"[PROFIT DEFICT] US${abs(number)} on day {day}")
+        file.writelines(f"[PROFIT DEFICT] US${abs(number)} on day {day}\n")
 
 
