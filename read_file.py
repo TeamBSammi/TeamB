c870762a9f7ec9ec5daa015a8e78555627472e7d
teamblokejiayu <s10242353@connect.np.edu.sg>
2022-08-03 17:09:29 +0800

    -

--- read_file.py
+++ read_file.py
@@ -51,6 +51,19 @@ for number in range(0,5):
     
     diff.append(int(profit[pal1])-(int(profit[pal2])))
 
+txtfile = Path.cwd()/"summary_report.txt"
+txtfile.touch()
+
+with txtfile.open(mode = "w",newline = "") as file:
+    for day,number in enumerate(difference,45):
+        writer = csv.writer(file)
+        if number < 0:
+            writer.writerow(f"[CASH DEFICT] US${abs(number)} on day {day}")
+    for day,number in enumerate(diff,45):
+        writer = csv.writer(file)
+        if number < 0:
+            writer.writerow(f"[PROFIT DEFICT] US${abs(number)} on day {day}")
+
 
 
 #reading of overheads
