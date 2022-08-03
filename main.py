b57cf36f2220c3785e3e0d40e399472ba610c8e9
team2athena <s10247682@connect.np.edu.sg>
2022-08-03 15:29:05 +0800

    name changes

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import api
 import cash_on_hand
 import overheads
-import profit_loss
-
+import profit_and_loss
 def main():
     forex = api.api_function()
     overheads.overheads_function(forex)
     cash_on_hand.coh_function(forex)
-    profit_loss.profitloss_function(forex)
+    profit_and_loss.profitloss_function(forex)
 
 main()
