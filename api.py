903d3a677f117de8f0f59a580a9c21da94d857a7
teamB_jialing <s10221087@connect.np.edu.sg>
2022-08-04 00:14:34 +0800

    api function

--- api.py
+++ api.py
@@ -6,11 +6,21 @@ import re, csv
 url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=RRIW0YM0PRPMKD9Q'
 response = requests.get(url)
 data = response.json()
+fp = Path.cwd()/'summary_report.txt'
 
-for item in data.values():
-    rate = (item["5. Exchange Rate"])
+def api_function():
+    for item in data.values():
+        forex = (item["5. Exchange Rate"])
+        forex = float(forex)
+    if fp.exists():
+        with fp.open(mode = "w",encoding = "utf-8",newline = "") as file:
+           file.writelines(f"[REAL TIME CURRENCY CONVERSION RATE] US$1 = {forex}")
+    return forex
 
-apirate = float(rate)
+#for item in data.values():
+    #rate = (item["5. Exchange Rate"])
+
+#apirate = float(rate)
 
 #txtfile = Path.cwd()/"summary_report.txt"
 #txtfile.touch()
