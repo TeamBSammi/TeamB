c50bc1ff37253e448e18e7f35d4c494c260b04cd
team2athena <s10247682@connect.np.edu.sg>
2022-08-03 17:40:33 +0800

    -

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+# importing each files as modules to coordinate & execute functions
 import api
-import read_file
-import write_file
+import cash_on_hand
+import overheads
+import profit_and_loss
 
+# creating all functions for each modules in the main file
 def main():
     forex = api.api_function()
-    read_file.read_file_function(forex)
-    write_file.write_file_function(forex)
+    overheads.overheads_function(forex)
+    cash_on_hand.coh_function(forex)
+    profit_and_loss.profitloss_function(forex)
 
+# execute
 main()
