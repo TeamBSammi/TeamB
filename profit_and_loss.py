b48e1f273899f135fa8b92e51029fcaf0a27bd5e
ewnice <eunicechua72@gmail.com>
2022-08-04 16:20:22 +0800

    PAL

--- profit_and_loss.py
+++ profit_and_loss.py
@@ -28,40 +28,10 @@ def profitandloss_function(forex):
     with txtfile.open(mode = "a",newline = "",encoding="utf-8") as file:
         for day,number in enumerate(difference,45):
             if number < 0:
-                file.writelines(f"[PROFTI DEFICT] SGD${(abs(number)*forex)} on day {day}\n")
+                file.writelines(f"[PROFTI DEFICT]  DAY: {day}, AMOUNT: {(abs(number)*forex)}\n")
             elif not number < 0:
                 file.writelines(f"[PROFIT SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n")
                 break
 
 
 profitandloss_function(forex)
-
-# file_palpath = Path.cwd()/"csv_reports"/"profit-and-loss-usd.csv"
-# fp = Path.cwd()/'summary_report.txt'
-# fp.touch()
-
-# def profitloss_function(forex):
-#     with file_palpath.open(mode = "r", encoding = "utf-8") as filepal:
-#         csvreaderpal = csv.reader(filepal)
-#         next(csvreaderpal)
-
-#     day = []
-#     profit = []
-        
-#     for values in csvreaderpal:
-#         day.append(values[0])
-#         profit.append(values[4])
-#     pal1 = 0
-#     pal2 = -1
-#     diff = []
-
-#     for number in range(0,5):
-#         pal1 += 1
-#         pal2 += 1
-        
-#     diff.append(int(profit[pal1])-(int(profit[pal2])))
-
-#     with fp.open(mode = "w",newline = "") as file:
-#         for day,number in enumerate(diff,45):
-#             if number < 0:
-#                 file.writelines(f"[PROFIT DEFICT] US${abs(number)} on day {day}\n")
