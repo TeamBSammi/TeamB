1717279c405069b7691a24339d0a4c1b628ed76b
teamblokejiayu <s10242353@connect.np.edu.sg>
2022-08-04 12:10:00 +0800

    overheads

--- overheads.py
+++ overheads.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 import re, csv
-
+from api import forex
 
 #reading of overheads
 file_opath = Path.cwd()/"csv_reports"/"overheads-day-40.csv"
 txtfile = Path.cwd()/"summary_report.txt"
 txtfile.touch()
 
-def overheads_function(forex):
+def overheads_function():
     with file_opath.open(mode = "r", encoding = "utf-8") as fileread:
         csvreadero= csv.reader(fileread)
         next(csvreadero)
@@ -20,15 +20,18 @@ def overheads_function(forex):
             cat= max(values)
             amount=[]
             for num in amount:
-                amount.append(overheads_function(forex))
                 maximum = (max(overheads))
                 cat= max(values)
                 
-    with txtfile.open(mode = "w",newline = "") as fileo:
-            for num in overheads:
-                fileo.writelines(f"[HIGHEST OVERHEADS] {cat}: {maximum}")
+    # with txtfile.open(mode = "w",newline = "") as fileo:
+    #         for num in overheads:
+    #             fileo.writelines(f"[HIGHEST OVERHEADS] {cat}: {maximum}")
                 
-        
+    with txtfile.open(mode = "w",newline = "") as file:
+        for num in overheads: 
+             file.writelines(f"[HIGHEST OVERHEADS] {cat}: {(abs(maximum)*forex)}")
+
+overheads_function()  
         
         # #for loop is used to iterate the elements
         # for tenant in combine:
