fdc6e047f20dcc316514f97eae6b6dfdfde8cc8e
teamblokejiayu <s10242353@connect.np.edu.sg>
2022-08-04 09:08:23 +0800

    overheads

---
+++ overheads.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import re, csv
+
+
+#reading of overheads
+file_opath = Path.cwd()/"csv_reports"/"overheads-day-40.csv"
+txtfile = Path.cwd()/"summary_report.txt"
+txtfile.touch()
+
+def overheads_function(forex):
+    with file_opath.open(mode = "r", encoding = "utf-8") as fileread:
+        csvreadero= csv.reader(fileread)
+        next(csvreadero)
+        
+        category = []
+        overheads = []
+        for values in csvreadero:
+            category.append([0])
+            overheads.append([1])
+            cat= max(values)
+            # maxoverheads= max(overheads)
+            amount=[]
+            for num in amount:
+                amount.append(overheads_function(forex))
+                maximum = (max(overheads))
+                cat= max(values)
+                # lines= (f"[HIGHEST OVERHEADS] {cat}: {maxover}")
+                
+    with txtfile.open(mode = "w",newline = "") as fileo:
+            for category, overheads in overheads:
+                fileo.writelines(f"[HIGHEST OVERHEADS] {cat}: {maximum}")
+        
+        
+        
+        # #for loop is used to iterate the elements
+        # for tenant in combine:
+        #     # .append() it is used to add a single item to the list 
+        #     empty.append(tenant)
+        #     # Use .writerow() to write single line and iterate over a for loop.
+        #     writer.writerow(tenant)
