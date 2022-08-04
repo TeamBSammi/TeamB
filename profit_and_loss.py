e93f28d88dde7bfb9136d505b5c0581212d3bc2f
ewnice <eunicechua72@gmail.com>
2022-08-04 21:33:40 +0800

    profitanloss

--- profit_and_loss.py
+++ profit_and_loss.py
@@ -1,5 +1,8 @@
+#Import path to start out the code which provides an object for working with files and directories.
 from pathlib import Path
+# import re is to set specifies set of stringd that matches it and import csv is to create a object from items in csv
 import re, csv
+
 from api import forex
 
 file_cohpath = Path.cwd()/"csv_reports"/"profit-and-loss-usd.csv"
@@ -28,7 +31,7 @@ def profit_and_loss_function(forex):
     with txtfile.open(mode = "a",newline = "",encoding="utf-8") as file:
         for day,number in enumerate(difference,45):
             if number < 0:
-                file.write(f"[PROFTI DEFICT]  DAY: {day}, AMOUNT: {(abs(number)*forex)}\n")
+                file.write(f"[PROFIT DEFICT]  DAY: {day}, AMOUNT: {(abs(number)*forex)}\n")
             else:
                 if min(difference) > 0:
                     file.write(f"[PROFIT SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n")
