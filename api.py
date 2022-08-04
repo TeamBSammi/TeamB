74569e40f04f1a206940d4ae87d03565e3d6cbd9
TeamBSammi <s10247843@connect.np.edu.sg>
2022-08-04 12:13:35 +0800

    api

--- api.py
+++ api.py
@@ -9,14 +9,13 @@ response = requests.get(url)
 data = response.json()
 fp = Path.cwd()/'summary_report.txt'
 
-
+for item in data.values():
+    forex = float(data['Realtime Currency Exchange Rate']["5. Exchange Rate"])
 
 def api_function():
-    for item in data.values():
-        forex = float(data['Realtime Currency Exchange Rate']["5. Exchange Rate"])
-        if fp.exists():
-            with fp.open(mode ="w", encoding = "utf-8",newline = "") as file:
-                file.writelines(f"[REAL TIME CURRENCY CONVERSION RATE] US$1 = {forex}")
+    if fp.exists():
+        with fp.open(mode ="w", encoding = "utf-8",newline = "") as file:
+            file.writelines(f"[REAL TIME CURRENCY CONVERSION RATE] US$1 = {forex}")
 
 api_function()
 
